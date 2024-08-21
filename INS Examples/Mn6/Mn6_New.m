10ca477ad01d688d9f044e8a24b07dc9dc101e26
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2024-08-21 12:27:23 +0100

    Update Mn6_New.m

--- INS Examples/Mn6/Mn6_New.m
+++ INS Examples/Mn6/Mn6_New.m
@@ -5,7 +5,7 @@ kelvin = rcm*0.695;  % kelvin        to MHz
 meV = rcm*8.065;     % meV           to MHz
 Tesla = meV*0.116;   % Tesla         to MHz
 SysOut1=[];
-%% The measured eigenvalues are
+% The measured eigenvalues are
 exp_eigs = [0.14153, 0.59095, 0.59095, 0.9428, 0.9428, 0.9428, 1.2285+0.59095,1.2285+0.59095, 1.5128+0.9428, 2.603, 2.86];
 exp_errs = [0.00016, 0.00011, 0.0003, sqrt(0.0016^2+0.00011^2), sqrt(0.0016^2+0.0003^2), 0.012, 0.02];
 % exp_eigs = [0.14153, 0.59095, 0.9428, 1.2285+0.59095, 1.5128+0.9428, 2.86];
@@ -22,7 +22,7 @@ exp_errs = [0.00016, 0.00011, 0.0003, sqrt(0.0016^2+0.00011^2), sqrt(0.0016^2+0.
 % exp_eigs = [ 0, 0.1414, 0.59070, 0.59070, 1.0841, 1.0841 , 1.0841, 1.4134, 1.4134, 2.316, 2.316, 2.316, 2.316, 2.316,  2.5218, 2.5218];
 
 
-%% Here are some somewhat reasonable starting parameters
+% Here are some somewhat reasonable starting parameters
 clear Sys Exp Vary
 Sys.S = [2 2 5/2 5/2 5/2 5/2]; % MnIII has S = 2, MnII has S = 5/2
 
@@ -37,15 +37,15 @@ J_AB = J_S4_S4;
 J_A1 = J_S4_S5_1; J_A3 = J_S4_S5_1; J_B2 = J_S4_S5_1; J_B4 = J_S4_S5_1; %For these, the MnIII JT axis is involved. Can be FM or AFM
 J_A2 = J_S4_S5_2; J_A4 = J_S4_S5_2; J_B1 = J_S4_S5_2; J_B3 = J_S4_S5_2; %For these, the MnIII JT axis is NOT involved. Can only be AFM
 J_12 = J_S5_S5;   J_34 = J_S5_S5; 
-J_14 = 0;         J_23 = 0; %Assumed zero. Only interacts via a pivalate bridge.
+J_14 = -0.01.*meV;         J_23 = J_14; %Assumed zero. Only interacts via a pivalate bridge.
 J_13 = 0;         J_24 = 0; %Assumed zero. No interaction pathway
 
-%Trial
-J_S4_S4   = -5*meV;    % MnIII - MnIII.                       Rodolphe: Strong and AFM.    Keep fixed.
-J_S4_S5_1 = 0.1*meV;  % MnIII - MnII, MnIII JT involved.     Rodolphe: Weak, FM or AFM.   Free value
-J_S4_S5_2 = -0.3*meV; % MnIII - MnII, MnIII JT not involved. Rodolphe: Weak and AFM.      Free value
-J_S5_S5   = -0.05*meV;  % MnII  - MnII.  
-J_14 = -0.01;         J_23 = -0.001; %Assumed zero. Only interacts via a pivalate bridge.
+% %Trial
+% J_S4_S4   = -5*meV;    % MnIII - MnIII.                       Rodolphe: Strong and AFM.    Keep fixed.
+% J_S4_S5_1 = 0.1*meV;  % MnIII - MnII, MnIII JT involved.     Rodolphe: Weak, FM or AFM.   Free value
+% J_S4_S5_2 = -0.3*meV; % MnIII - MnII, MnIII JT not involved. Rodolphe: Weak and AFM.      Free value
+% J_S5_S5   = -0.05*meV;  % MnII  - MnII.  
+% J_14 = -0.01;         J_23 = -0.001; %Assumed zero. Only interacts via a pivalate bridge.
 
 
 
@@ -56,11 +56,11 @@ Sys.J = [J_AB J_A1 J_A2 J_A3 J_A4 J_B1 J_B2 J_B3 J_B4 J_12 J_13 J_14 J_23 J_24 J
 % For scenario 1, these stay constant. Middle/green ones
 
 DIII = -0.029*meV; % MnIII anisotropy. Free Value 
-DIII = -0.028*meV; % MnIII anisotropy. Free Value 
+% DIII = -0.028*meV; % MnIII anisotropy. Free Value 
 EIII = 0*DIII;   % MnIII rhombicity. For INS, assume 0.
 B20III = 3*DIII; B22III = EIII; %Converting to Stevens Operator formalism
 
-DII = -0.000*meV; % MnII anisotropy. For INS, assume 0. 
+DII = -0.001*meV; % MnII anisotropy. For INS, assume 0. 
 EII = DII*0;      % MnII rhombicity. For INS, assume 0.
 B20II = 3*DII; B22II = EII; %Converting to Stevens Operator formalism
 
@@ -70,15 +70,28 @@ Sys.B2 = [B22III 0 B20III 0 0;
           B22II 0 B20II 0 0;
           B22II 0 B20II 0 0;
           B22II 0 B20II 0 0];
+% Sys.B4=  [0 0 0 0 0.01*meV 0 0 0 0;
+%           0 0 0 0 0.01*meV 0 0 0 0;
+%           0 0 0 0 0 0 0 0 0;
+%           0 0 0 0 0 0 0 0 0;
+%           0 0 0 0 0 0 0 0 0;
+%           0 0 0 0 0 0 0 0 0];
 
 %set the parameters to be varied
 Vary.B2= [0 0 1 0 0;
           0 0 1 0 0;
-          0 0 0 0 0;
-          0 0 0 0 0;
-          0 0 0 0 0;
-          0 0 0 0 0];
-Vary.J = [0 1 1 1 1 1 1 1 1 1 0 1 1 0 1]; 
+          0 0 1 0 0;
+          0 0 1 0 0;
+          0 0 1 0 0;
+          0 0 1 0 0];
+% Vary.B4= [0 0 0 0 1 0 0 0 0;
+%           0 0 0 0 1 0 0 0 0;
+%           0 0 0 0 0 0 0 0 0;
+%           0 0 0 0 0 0 0 0 0;
+%           0 0 0 0 0 0 0 0 0;
+%           0 0 0 0 0 0 0 0 0];
+% Vary.J = [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]; 
+Vary.J = Sys.J;   Vary.J(1) = 0;
 
  % NumEigs = 12;    %Number of experimental eigenvalues to be fit
  % NumEigs = length(exp_eigs); 
@@ -87,18 +100,18 @@ Exp.ev = exp_eigs.*meV;
 
 %%
 
-Opt = struct('NDeflations',1,'Method','LP','Linesearch','Quadratic',...
-    'MaxIter',2000,'StepTolerance',1e-6,'GradientTolerance',1e-8,...
+Opt = struct('NDeflations',1,'Method','Good_GN','Linesearch','Quadratic',...
+    'MaxIter',1000,'StepTolerance',1e-6,'GradientTolerance',1e-8,...
     'ObjectiveTolerance',1e-2,'Scaled',false,'epsilon',0.1,'MinAlpha',1e-18,...
-    'IEPType','Classic','Verbose',true,'c1',1e-8,'SysFound',[],...
-    'Regularisation',0,'LinearSolver','mldivide','Alpha0',1e-0);
+    'IEPType','Difference','Verbose',true,'c1',1e-8,'SysFound',[],...
+    'Regularisation',[],'LinearSolver','lsqminnorm','Alpha0',1e-0);
 
-SysOut = INS_IEP(Sys,Vary,Exp,Opt)
+[SysOut, options] = INS_IEP(Sys,Vary,Exp,Opt)
 
 %%       
 % INS-specific information. Magnetic form factors.
-MintSys = Sys(1);
-% MintSys = Sys;
+MintSys = SysOut(1);
+MintSys = Sys;
 MintSys.FormFactor = {'Mn3', 'Mn3', 'Mn2', 'Mn2', 'Mn2', 'Mn2'};
 
 % Metal ion coordinates for INS. Only relative positions are important.
@@ -113,7 +126,7 @@ MintSys.Coords = [24.60550  13.06674   7.07523; % A
 
 % Sim INS powder spectrum
 
-MintOpt.NumEigs = 33; %100 eigenvalues gives a good INS sim
+MintOpt.NumEigs = 100; %100 eigenvalues gives a good INS sim
 
 MintExp.SpectrumType = 'SE'; %INS intensity vs. energy
 
