c209731d6ec8bb635daf4e30daf19409681121cd
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-06-11 13:09:36 +0100

    added dense option

--- Component Formation/FormA.m
+++ Component Formation/FormA.m
@@ -7,46 +7,76 @@ function Ax = FormA(x,A,varargin)
 if nargin>3
     error("incorect number of inputs")
 end
-if iscell(A)
-I = [];
-J = [];
-V = [];
-n = 0;
-for k = 1:length(A)
-    [i,j,v] = find(A{k});
-    p = n + numel(i);
-    m = numel(I);
-    if p > m
-        m = max(p,2*m);
-        I(m) = 0;
-        J(m) = 0;
-        V(m) = 0;
-    end
-    idx = (n+1:p);
-    I(idx) = i;
-    J(idx) = j;
-    V(idx) = v*x(k);
-    n = p;
-end
-idx = (n+1:numel(I));
-I(idx) = [];
-J(idx) = [];
-V(idx) = [];
-[m,n] = size(A{1});
-Ax = sparse(I,J,V,m,n);
 
-if nargin == 3
-    Ax = varargin{1}+Ax;
-end
+
+if iscell(A)
+    Aisdesnse = true;
+    if nargin == 3
+        if issparse(varargin{1})
+            Aisdesnse = false;
+        end
+    end
+    if Aisdesnse
+        for i = 1:length(A)
+            if issparse(A{i})
+                Aisdesnse = false;
+                break
+            end
+        end
+    end
+    
+    if Aisdesnse
+        if nargin == 3
+            Ax = varargin{1};
+        else
+            Ax = zeros(size(A{1}));
+        end
+        for i = 1:length(A)
+            Ax = Ax + A{i}*x(i);
+        end
+    else
+
+        I = [];
+        J = [];
+        V = [];
+        n = 0;
+        for k = 1:length(A)
+            [i,j,v] = find(A{k});
+            p = n + numel(i);
+            m = numel(I);
+            if p > m
+                m = max(p,2*m);
+                I(m) = 0;
+                J(m) = 0;
+                V(m) = 0;
+            end
+            idx = (n+1:p);
+            I(idx) = i;
+            J(idx) = j;
+            V(idx) = v*x(k);
+            n = p;
+        end
+        idx = (n+1:numel(I));
+        I(idx) = [];
+        J(idx) = [];
+        V(idx) = [];
+        [m,n] = size(A{1});
+        Ax = sparse(I,J,V,m,n);
+
+        if nargin == 3
+            Ax = varargin{1}+Ax;
+        end
+    
+    end
 elseif isstruct(A)
 
-lidx =1; v = zeros(sum(A.NumV),1);
+    lidx =1; v = zeros(sum(A.NumV),1);
     for k = 1:length(x)
         uidx = lidx+A.NumV(k)-1;
         v(lidx:uidx) = A.v(lidx:uidx)*x(k);
         lidx =uidx+1;
     end
-Ax = sparse(A.i,A.j,v,A.m,A.n);
+    Ax = sparse(A.i,A.j,v,A.m,A.n);
 
 else
     error("A must be a cell array of basis matrices, or a structure used to construct a sparse matrix")
