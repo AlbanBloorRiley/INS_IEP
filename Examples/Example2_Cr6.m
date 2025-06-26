44161bc2f70cd39fc3be389e613707b7632d5a17
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-06-26 15:40:50 +0100

    Added comment about pinning

--- Examples/Example2_Cr6.m
+++ Examples/Example2_Cr6.m
@@ -28,7 +28,7 @@ H = ham(ExpSys,[0,0,0]);
 
 Exp.ev = [0,0.355,0.457,0.497,1.576,1.577,1.592,1.629,1.632,2.97,2.98,3.002,3.004,3.01,3.038,3.821,3.824,3.827,3.837,3.856,3.879,3.888,3.895,3.903];
 %% Set up initial guess 
-
+% Since it is known a priori that each spin centre will have the same value parameters we will pin the parameters here, by setting the initial gues as the same value
 Sys1.S = Sys.S;
 Sys1.B2 = Sys.B2.*[1 0 -1 0 0];
 % Sys1.B2 =  [1     0    -1     0     0;
@@ -58,6 +58,11 @@ end
 Sys2.ee = ee.*1e2;
 
 %Note that it is not possible to use both  Sys.J and Sys.ee at the same time
+%When Sys.ee is Nx3 parameters can be pinned as before by inputting the
+%same initial guess values, when Sys.ee is 3Nx3 this can be done for the
+%diagonal entries and off diagonal entries if the input guess is skew
+%symmetric. 
+
 Sys2 = rmfield(Sys2,'J');
 %and Vary has to be updated: 
 Vary2 =Sys2;
@@ -74,7 +79,6 @@ all(all(ham(SysOut1,[0,0,0]) == ham(SysOut2,[0,0,0])))
 
 Opt = struct('NDeflations',4,'Sigma',1e-7);
 SysOut= INS_IEP(Sys1,Vary1,Exp,Opt);
-
 
 
 
