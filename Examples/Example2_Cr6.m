6bb50fee09a65d3fc4b07d177b969d912d6a1f6d
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-07-03 14:05:23 +0100

    Update Example2_Cr6.m

--- Examples/Example2_Cr6.m
+++ Examples/Example2_Cr6.m
@@ -26,25 +26,26 @@ ExpSys.B2 = Sys.B2.*[0.007.*meV 0 (-0.041/3).*meV 0 0];
 H = ham(ExpSys,[0,0,0]);
 [~,E]=eig(H); EE = diag(E);  Exp.ev=EE(1:24)-EE(1);
 
-Exp.ev = [0,0.355,0.457,0.497,1.576,1.577,1.592,1.629,1.632,2.97,2.98,3.002,3.004,3.01,3.038,3.821,3.824,3.827,3.837,3.856,3.879,3.888,3.895,3.903];
+Exp.ev = [0,0.355,0.457,0.497,1.576,1.577,1.592,1.629,1.632,2.97,2.98,3.002,3.004,3.01,3.038,3.821,3.824,3.827,3.837,3.856,3.879,3.888,3.895,3.903]'.*meV;
 %% Set up initial guess 
 % Since it is known a priori that each spin centre will have the same value parameters we will pin the parameters here, by setting the initial gues as the same value
 Sys1.S = Sys.S;
-Sys1.B2 = Sys.B2.*[1 0 -1 0 0];
-% Sys1.B2 =  [1     0    -1     0     0;
-%            1     0    -1     0     0;
-%            1     0    -1     0     0;
-%            1     0    -1     0     0;
-%            1     0    -1     0     0;
-%            1     0    -1     0     0];
-Sys1.J = Sys.J*1e2;
-% Sys1.J = [100,0,0,0,0,100,0,0,0,100,0,0,100,0,100];
+% Sys1.B2 = Sys.B2.*[1 0 -1 0 0];
+Sys1.B2 =  [1     0    -1     0     0;
+           1     0    -1     0     0;
+           1     0    -1     0     0;
+           1     0    -1     0     0;
+           1     0    -1     0     0;
+           1     0    -1     0     0];
+% Sys1.J = Sys.J*1e2;
+Sys1.J = [100,0,0,0,0,100,0,0,0,100,0,0,100,0,100];
 
 
 Vary1 = Sys;
 
-% Calculate the minimising system: 
-SysOut1= INS_IEP(Sys1,Vary1,Exp);
+% Calculate the minimising system:
+Opt.StepTolerance = 1e-4;
+SysOut1= INS_IEP(Sys1,Vary1,Exp,Opt);
 
 
 %% Using Sys.ee
@@ -66,8 +67,8 @@ Sys2.ee = ee.*1e2;
 Sys2 = rmfield(Sys2,'J');
 %and Vary has to be updated: 
 Vary2 =Sys2;
-
-SysOut2= INS_IEP(Sys2,Vary2,Exp);
+Opt.StepTolerance = 1e-4;
+SysOut2= INS_IEP(Sys2,Vary2,Exp,Opt);
 
 %Check that the output structures are equivalent:
 all(all(ham(SysOut1,[0,0,0]) == ham(SysOut2,[0,0,0])))
@@ -77,7 +78,8 @@ all(all(ham(SysOut1,[0,0,0]) == ham(SysOut2,[0,0,0])))
 %change one of the deflation parmeters to make the deflation a little more 
 % sensitive. 
 
-Opt = struct('NDeflations',4,'Sigma',1e-7);
+Opt = struct('NDeflations',5,'Sigma',1e-7,'StepTolerance',1e-3);
+Opt.verbose = true
 SysOut= INS_IEP(Sys1,Vary1,Exp,Opt);
 
 
